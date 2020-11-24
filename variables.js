b4ed5c2e7cfdf96bfc306baf4a5c45553bb7f268
NotKcaHDer <62846928+NotKcaHDer@users.noreply.github.com>
2020-11-24 20:52:19 +0500

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,32 @@
+// переменная это коробка которая хранит значение в коде 
+// обьявляется переменная с помощью let, var, const
+// и имя переменной
+// let myVariable; //создал переменную 
+// myVariable = 67; //присвоил значение 
+let myVariable = 67; //это короткий метод (так лучше)
+// константы
+// если обьявить переменную с помощью конст, то она будет неизменяемая
+// константы обьявляются заглавными буквами
+const MYVARIABLE = 35;
+myVariable = 100;
+
+console.log(myVariable);
+console.log(MYVARIABLE);
+const a = 15;
+const b = 7;
+// арифметические операторы
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a%b); //остаток от деления
+// строки
+// это набор имволов внутри двойных или одинарных кавычек
+let myString = 'hello';
+console.log(myString);
+// конкатенация или сложение строк 
+let a1 = 'word';
+let b1 = 'ok';
+console.log(b1+a1+'привет я Семен');
+// булевые значения
+let pravda = true;
